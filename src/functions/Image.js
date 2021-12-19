54e9375ca63d47346d6697acabbf2a4302a94df3
Erik Olsen <tusamni@gmail.com>
2021-12-18 22:02:53 -0600

    commit

--- src/functions/Image.js
+++ src/functions/Image.js
@@ -1,13 +1,22 @@
-import sizeOf from 'image-size';
 import settings from '../data/settings.json'
+import fs from 'fs'
+import path from 'path';
+import sizeOf from 'image-size';
+import exifr from 'exifr'
 
+
+// build the public image url
+// relative to the web root
 export function buildPublicPath(file) {
     return `/${settings.image.directory.public}${file}`;
 }
+// build the local path
+// relative to the 'public' directory
 export function buildLocalPath(file) {
     return `${settings.image.directory.local}${file}`;
 }
 
+// get the width and height dimensions of an image
 export function getDimensions(file) {
     const localPath = buildLocalPath(file);
 
@@ -15,4 +24,38 @@ export function getDimensions(file) {
     const dimensions = sizeOf(localPath);
 
     return dimensions;
+}
+
+export async function getMetadata(file) {
+    const localPath = buildLocalPath(file);
+
+    // collect all the exif, iptc and ifd0 data from the image
+    const exifrOptions = {
+        iptc: ['ObjectName', 'Caption', 'Byline'],
+        exif: ['LensModel', 'FNumber', 'ISO', 'ExposureTime', 'DateTimeOriginal', 'FocalLength'],
+        ifd0: ['Make', 'Model'],
+        gps: false,
+        mergeOutput: true,
+    }
+    // read the image into fs
+    const fsFile = fs.readFileSync(localPath); // read the image
+    const metadata = await exifr.parse(fsFile, exifrOptions);
+
+    // return the array to hold all the image data
+    //if (this.data == "all") {
+    return {
+        "filename": path.basename(localPath, '.jpg'),
+        "date": metadata.DateTimeOriginal,
+        "title": metadata.ObjectName,
+        "caption": metadata.Caption,
+        "author": metadata.Byline,
+        "cameraMake": metadata.Make,
+        "cameraModel": metadata.Model,
+        "lens": metadata.LensModel,
+        "aperture": metadata.FNumber,
+        "iso": metadata.ISO,
+        "focalLength": metadata.FocalLength,
+        "exposure": 1 / metadata.ExposureTime,
+    };
+    //}
 }
