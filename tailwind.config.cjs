6957fb409dd3c1e64ba5dd1af18e39979451ddf1
Erik Olsen <tusamni@gmail.com>
2022-02-18 16:55:22 -0600

    commit

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -12,5 +12,8 @@ module.exports = {
                 sans: ['Inter var', ...defaultTheme.fontFamily.sans],
             }
         }
-    }
+    },
+    plugins: [
+        require('@tailwindcss/typography'),
+    ],
 };
