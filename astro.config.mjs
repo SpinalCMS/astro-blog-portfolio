b9f8cc9b46689b6b2fc0a54b407493c43baecd25
Erik Olsen <tusamni@gmail.com>
2022-04-13 16:57:01 -0500

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,4 +12,10 @@ export default defineConfig({
             },
         }),
     ],
+    markdown: {
+        shikiConfig: {
+            theme: 'dracula',
+            wrap: true,
+        },
+    },
 });
